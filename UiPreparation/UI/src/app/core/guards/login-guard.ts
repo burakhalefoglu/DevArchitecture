8a6044fac213bd07047c6e53d53847154b35b33a
Burak Halefoğlu <burakhalefoglu@gmail.com>
2024-07-11 13:07:56 +0300

    angular update 15 -> 16

--- UiPreparation/UI/src/app/core/guards/login-guard.ts
+++ UiPreparation/UI/src/app/core/guards/login-guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "../components/admin/login/services/auth.service";
 import { LocalStorageService } from "../services/local-storage.service";
 
 
 @Injectable()
-export class LoginGuard implements CanActivate {
+export class LoginGuard  {
 
     constructor(private router: Router, private authService: AuthService) { }
 
