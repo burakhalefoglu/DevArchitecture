ab5bf4341cf2179b64fc5056df8b42088afcf530
Burak Halefoğlu <burakhalefoglu@gmail.com>
2024-09-09 08:42:23 +0300

    Update confirmation_dialog.dart

--- FlutterUi/flutter_devarchitecture/lib/core/widgets/confirmation_dialog.dart
+++ FlutterUi/flutter_devarchitecture/lib/core/widgets/confirmation_dialog.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import '../theme/custom_colors.dart';
 
@@ -10,25 +9,40 @@ showConfirmationDialog(BuildContext c, void Function() onPressed) {
       return BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
         child: AlertDialog(
-          surfaceTintColor: CustomColors.white.getColor,
+          surfaceTintColor: CustomColors.danger.getColor
+              .withOpacity(0.8), // Tehlike rengi tonu
           backgroundColor: CustomColors.white.getColor,
           shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(20)),
           ),
-          title: const Text('Emin misiniz?'),
-          content: const Text('Bu işlem geri alınamaz.'),
+          title: Text(
+            'Emin misiniz?',
+            style: TextStyle(
+              color: CustomColors.danger.getColor, // Başlık için tehlike rengi
+              fontWeight: FontWeight.bold, // Kalın yazı tipi
+            ),
+          ),
+          content: const Text('Bu işlem geri alınamaz.'),
           actions: <Widget>[
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: const Text('İptal'),
+              style: TextButton.styleFrom(
+                foregroundColor:
+                    CustomColors.gray.getColor, // İptal için gri ton
+              ),
+              child: const Text('İptal'),
             ),
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
                 onPressed();
               },
+              style: TextButton.styleFrom(
+                foregroundColor:
+                    CustomColors.danger.getColor, // Evet için kırmızı ton
+              ),
               child: const Text('Evet'),
             ),
           ],
