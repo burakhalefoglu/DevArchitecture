8e4bfa7b00a0369721208e69f36ae27e397f7167
Burak Halefoğlu <burakhalefoglu@gmail.com>
2024-10-08 17:11:32 +0300

    Update modular_mixin.dart

--- FlutterUi/flutter_devarchitecture/lib/features/mixin/modular_mixin.dart
+++ FlutterUi/flutter_devarchitecture/lib/features/mixin/modular_mixin.dart
@@ -1,10 +1,11 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_devarchitecture/core/theme/theme_provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:provider/provider.dart';
-import '../../core/theme/theme_provider.dart';
 import '../../core/helpers/translation_provider.dart';
+import '../../routes/app_route_module.dart';
 
 mixin ModularMixin {
   Widget buildModular(BuildContext context) {
@@ -19,40 +20,42 @@ mixin ModularMixin {
       ],
       child: Consumer2<ThemeProvider, TranslationProvider>(
         builder: (context, themeProvider, translationProvider, child) {
-          print("Modular yüklendi!");
-          return MaterialApp.router(
-            locale: translationProvider.locale,
-            supportedLocales: const [
-              Locale('en', 'US'),
-              Locale('tr', 'TR'),
-              Locale('de', 'DE'),
-              Locale('es', 'ES'),
-              Locale('fr', 'FR'),
-              Locale('it', 'IT'),
-              Locale('pt', 'PT'),
-              Locale('ru', 'RU'),
-              Locale('ja', 'JP'),
-              Locale('zh', 'CN'),
-            ],
-            localizationsDelegates: const [
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            scrollBehavior: ScrollConfiguration.of(context).copyWith(
-              dragDevices: {
-                PointerDeviceKind.touch,
-                PointerDeviceKind.mouse,
-                PointerDeviceKind.stylus,
-                PointerDeviceKind.trackpad,
-                PointerDeviceKind.invertedStylus,
-                PointerDeviceKind.unknown,
-              },
+          return ModularApp(
+            module: AppRouteModule(),
+            child: MaterialApp.router(
+              locale: translationProvider.locale,
+              supportedLocales: const [
+                Locale('en', 'US'),
+                Locale('tr', 'TR'),
+                Locale('de', 'DE'),
+                Locale('es', 'ES'),
+                Locale('fr', 'FR'),
+                Locale('it', 'IT'),
+                Locale('pt', 'PT'),
+                Locale('ru', 'RU'),
+                Locale('ja', 'JP'),
+                Locale('zh', 'CN'),
+              ],
+              localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              scrollBehavior: ScrollConfiguration.of(context).copyWith(
+                dragDevices: {
+                  PointerDeviceKind.touch,
+                  PointerDeviceKind.mouse,
+                  PointerDeviceKind.stylus,
+                  PointerDeviceKind.trackpad,
+                  PointerDeviceKind.invertedStylus,
+                  PointerDeviceKind.unknown,
+                },
+              ),
+              theme: themeProvider.lightTheme,
+              darkTheme: themeProvider.darkTheme,
+              themeMode: themeProvider.themeMode,
+              routerConfig: Modular.routerConfig,
             ),
-            theme: themeProvider.lightTheme,
-            darkTheme: themeProvider.darkTheme,
-            themeMode: themeProvider.themeMode,
-            routerConfig: Modular.routerConfig,
           );
         },
       ),
