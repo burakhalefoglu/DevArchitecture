1594379984dfcac706a52d6c364bdb0ab0725c2f
Burak Halefoğlu <burakhalefoglu@gmail.com>
2025-01-27 17:38:31 +0300

    flutter feature added

---
+++ UiPreparation/FlutterUi/flutter_devarchitecture/.dart_tool/flutter_build/557650a56d29368473660f21e6e5e7c6/native_assets.d
@@ -0,0 +1,1 @@
+ C:\\Users\\IT\\Documents\\GitHub\\DevArchitecture\\UiPreparation\\FlutterUi\\flutter_devarchitecture\\.dart_tool\\flutter_build\\557650a56d29368473660f21e6e5e7c6\\native_assets.yaml: 
